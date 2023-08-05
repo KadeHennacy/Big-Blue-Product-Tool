e3488bf8f44a306756242813dacaa9542e6a8c3f
KadeHennacy <kade.hennacy@gmail.com>
2023-08-05 13:28:55 -0400

    Basic Nav Bar Setup

--- src/common/components/Topbar.tsx
+++ src/common/components/Topbar.tsx
@@ -1,80 +1,46 @@
-import { Box, IconButton, useTheme } from "@mui/material";
-import { useContext } from "react";
-import { ColorModeContext, tokens } from "../../../theme";
-// import logo from "../../assets/logo.svg";
-// import darkLogo from "../../assets/darkLogo.svg";
-import InputBase from "@mui/material/InputBase";
-import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import React, { useContext } from "react";
+import { AppBar, Toolbar, Button, IconButton } from "@mui/material";
+import { ColorModeContext } from "../../../theme";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
-import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
-import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import SearchIcon from "@mui/icons-material/Search";
-import styled from "@emotion/styled";
-import { useSelector } from "react-redux";
-import { Link, useLocation } from "react-router-dom";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import { useTheme } from "@mui/material/styles";
 
 const Topbar = () => {
-  const location = useLocation();
   const theme = useTheme();
-  const colors = tokens;
   const colorMode = useContext(ColorModeContext);
-  const StyledImg = styled.img`
-    max-width: 100%;
-    max-height: 100%;
-  `;
+  const buttonStyle = {
+    fontSize: "0.75rem",
+    width: "140px",
+    border: `1px solid ${theme.palette.primary.main}`,
+    borderRadius: "12px",
+    padding: "10px",
+    backgroundColor: theme.palette.primary.light,
+  };
 
   return (
-    <Box
-      component="div"
-      p={1}
-      px={3}
-      display="flex"
-      justifyContent="space-between"
-      bgcolor={theme.palette.primary.dark}
-      boxShadow={location.pathname === "/login" ? 5 : 0}
-      borderBottom={`2px solid ${theme.palette.primary.light}`}
-    >
-      <Box
-        display="flex"
-        maxHeight={50}
-        sx={{ width: "50%", objectFit: "contain" }}
+    <AppBar position="static">
+      <Toolbar
+        style={{ justifyContent: "space-around", padding: "0", fontSize: 12 }}
       >
-        {/* here is a link to / */}
-        <Link to="/">
-          <StyledImg
-            // src={theme.palette.mode === "dark" ? logo : darkLogo}
-            alt="logo"
-          />
-        </Link>
-      </Box>
-
-      {/* ICONS */}
-      <Box display="flex">
-        <IconButton onClick={colorMode.toggleColorMode}>
-          {theme.palette.mode === "dark" ? (
-            <DarkModeOutlinedIcon />
-          ) : (
-            <LightModeOutlinedIcon />
-          )}
-        </IconButton>
-        {/* {token ? (
-          <>
-            <IconButton>
-              <NotificationsOutlinedIcon />
-            </IconButton>
-            <IconButton>
-              <SettingsOutlinedIcon />
-            </IconButton>
-            <IconButton>
-              <PersonOutlinedIcon />
-            </IconButton>{" "}
-          </>
-        ) : (
-          <></>
-        )} */}
-      </Box>
-    </Box>
+        <div>
+          <Button color="inherit" sx={buttonStyle}>
+            Browse Previous Lots
+          </Button>
+        </div>
+        <div>
+          <Button color="inherit" sx={{ ...buttonStyle, marginRight: 1 }}>
+            Start New Lot
+          </Button>
+          <IconButton onClick={colorMode.toggleColorMode}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlinedIcon />
+            ) : (
+              <LightModeOutlinedIcon />
+            )}
+          </IconButton>
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 };
 
